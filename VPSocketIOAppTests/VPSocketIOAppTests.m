8915707f5e23c707c32206019e63341e241e3919
luoyongmeng <hosten_lym@163.com>
2021-04-15 15:49:22 +0800

    fix:完善工程

--- VPSocketIOAppTests/VPSocketIOAppTests.m
+++ VPSocketIOAppTests/VPSocketIOAppTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <VPSocketIO/RTCVPSocketIO.h>
 
 @interface VPSocketIOAppTests : XCTestCase
 
---
+++ framework/VPSocketIO.framework/Headers/RTCVPSocketIO.h
@@ -0,0 +1,23 @@
+//
+//  RTCVPSocketIO_iOS.h
+//  RTCVPSocketIO-iOS
+//
+//  Created by Vasily Popov on 9/26/17.
+//  Copyright © 2017 Vasily Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RTCVPSocketIO_iOS.
+FOUNDATION_EXPORT double RTCVPSocketIOVersionNumber;
+
+//! Project version string for RTCVPSocketIO_iOS.
+FOUNDATION_EXPORT const unsigned char RTCVPSocketIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RTCVPSocketIO_iOS/PublicHeader.h>
+
+#import "RTCVPSocketIOClient.h"
+#import "RTCVPSocketOnAckCallback.h"
+#import "RTCVPSocketIOClientProtocol.h"
+#import "RTCVPSocketLogger.h"
+
