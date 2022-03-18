9c50cd01cda210d0bac717d380d62b4bf2411de8
luoyongmeng <hosten_lym@163.com>
2022-03-18 12:52:19 +0800

    demo 修改完善

--- VPSocketIO/ViewController.m
+++ VPSocketIO/ViewController.m
@@ -7,93 +7,144 @@
 //
 
 #import "ViewController.h"
-#import <VPSocketIO/RTCVPSocketIO.h>
-
-
-@interface ClientSocketLogger : RTCVPSocketLogger
-
-@end
-
-@implementation ClientSocketLogger
-
--(void) log:(NSString*)message type:(NSString*)type
-{
-    NSLog(@"ClientSocket MESSAGE: %@", message);
+#import "RTCVPSocketIO.h"
+#import "RTCVPSocketAckEmitter.h"
+#ifndef WEAKSELF
+#define WEAKSELF __weak __typeof(&*self)weakSelf = self;
+#endif
+#ifndef STRONGSELF
+#define STRONGSELF __strong __typeof(&*weakSelf)strongSelf = weakSelf;
+#endif
+#ifndef dispatch_queue_async_safe
+#define dispatch_queue_async_safe(queue, block)\
+if (dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL) == dispatch_queue_get_label(queue)) {\
+block();\
+} else {\
+dispatch_async(queue, block);\
 }
+#endif
 
--(void) error:(NSString*)message type:(NSString*)type
-{
-    NSLog(@"ClientSocket ERROR %@", message);
-}
+#ifndef dispatch_main_async_safe
+#define dispatch_main_async_safe(block) dispatch_queue_async_safe(dispatch_get_main_queue(), block)
+#endif
 
--(void)dealloc {
-    
-}
+@interface ViewController ()
 
-@end
+@property(nonatomic, strong) RTCVPSocketIOClient *socket;
+@property(nonatomic,strong) dispatch_queue_t currentEngineProtooQueue;
 
-
-@interface ViewController () {
-    RTCVPSocketIOClient *socket;
-}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _currentEngineProtooQueue = dispatch_queue_create("com.vrv.mediasoupProtoo", DISPATCH_QUEUE_SERIAL);
+
     // Do any additional setup after loading the view, typically from a nib.
+   
+}
+- (IBAction)connectServer:(id)sender {
+    [self socketExample];
 }
 
-
+- (void)sendMessage:(NSDictionary*)message withMethod:(NSString*)method{
+    
+    WEAKSELF
+    dispatch_async(_currentEngineProtooQueue, ^{
+        @autoreleasepool {
+            __strong typeof(weakSelf) blockSelf = weakSelf;
+            if (!blockSelf.socket) {
+               
+                return;
+            }
+            RTCVPSocketOnAckCallback *callback = [blockSelf.socket emitWithAck:method items:@[message]];
+            [callback timingOutAfter:10 callback:^(NSArray *array) {
+                if ([array[0] isKindOfClass:[NSNull class]]) {
+                    NSLog(@"");
+                }
+            }];
+        }
+        
+    });
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)buttonClicked:(id)sender {
+- (IBAction)sendMsg:(id)sender {
     
-    [self socketExample];
+    [self sendMessage:@{} withMethod:@"online"];
     
 }
 - (IBAction)disconnect:(id)sender {
     
-    [socket removeAllHandlers];
-    [socket disconnect];
-    self->socket = nil;
+    [self.socket removeAllHandlers];
+    [self.socket disconnect];
+    self.socket = nil;
     
 }
 
 -(void)socketExample
 {
-    ClientSocketLogger*logger = [ClientSocketLogger new];
-
-    NSString *urlString = @"http://localhost:8900";
+    NSString *urlString = @"http://192.168.140.184:8000";
     NSDictionary *connectParams = @{@"key":@"value"};
-    self->socket = [[RTCVPSocketIOClient alloc] init:[NSURL URLWithString:urlString]
-                                 withConfig:@{@"log": @YES,
-                                              @"forcePolling": @NO,
-                                              @"secure": @YES,
-                                              @"forceNew":@YES,
-                                              @"forceWebsockets":@YES,
-                                              @"selfSigned":@YES,
-                                              @"reconnectWait":@1000,
-                                              @"nsp":@"/rooms",
-                                              @"connectParams":connectParams,
-                                              @"logger":logger
-                                              }];
-    
-    
-    [socket on:kSocketEventConnect callback:^(NSArray *array, RTCVPSocketAckEmitter *emitter) {
-        NSLog(@"!!!!socket connected");
-    }];
-    
-    [socket on:@"startGame" callback:^(NSArray *data, RTCVPSocketAckEmitter *emitter) {
-        if(data.count > 0)
-        {
+    RTCVPSocketLogger *logger = [[RTCVPSocketLogger alloc]init];
+    [logger onLogMsgWithCB:^(NSString *message, NSString *type) {
+        if ([type isEqualToString:@"RTCVPSocketIOClient"]) {
+            //忽略消息
+            return;
+        }else if ([type isEqualToString:@"SocketParser"] && [message containsString:@"Decoded packet as"]){
+            return;
         }
+        
+    }];
+    self.socket = [[RTCVPSocketIOClient alloc] init:[NSURL URLWithString:urlString]
+                                    withConfig:@{@"log": @YES,
+                                                 @"reconnects":@YES,
+                                                 @"reconnectAttempts":@(20),
+                                                 @"forcePolling": @NO,
+                                                 @"secure": @NO,
+                                                 @"forceNew":@YES,
+                                                 @"forceWebsockets":@YES,
+                                                 @"selfSigned":@NO,
+                                                 @"reconnectWait":@3,
+                                                 @"nsp":@"/",
+                                                 @"connectParams":connectParams,
+                                                 @"logger":logger
+                                    }];
+   WEAKSELF
+    [_socket on:kSocketEventConnect callback:^(NSArray *array, RTCVPSocketAckEmitter *emitter) {
+        STRONGSELF
+        NSLog(@"====> connect msg:%@",array);
+    }];
+    [_socket on:kSocketEventDisconnect callback:^(NSArray *array, RTCVPSocketAckEmitter *emitter) {
+        STRONGSELF
+//        network change
+        dispatch_main_async_safe(^{
+            if (array.count > 1) {
+                if ([array containsObject:@"network change"]) {
+                    return;
+                }
+            }else{
+                if ([array containsObject:@"network change"]) {
+                    return;
+                }
+            }
+        });
+    }];
+    [_socket on:kSocketEventError callback:^(NSArray *array, RTCVPSocketAckEmitter *emitter) {
+        STRONGSELF
+       
     }];
     
-    [socket connect];
+    
+    [_socket connectWithTimeoutAfter:10 withHandler:^{
+        STRONGSELF
+       
+       
+    }];
     
 }
 
